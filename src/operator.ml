c54455c20adf267b30c3ed70d860efe20e98a226
Ayberk Tosun <ayberk.tosun@gmail.com>
2017-05-12 02:43:05 -0500

    Adapt OPERATOR to sorts

--- src/operator.ml
+++ src/operator.ml
@@ -1,6 +1,11 @@
 module type OPERATOR = sig
-  type t
-  val arity : t -> int list
-  val equal : t * t -> bool
-  val to_string : t -> string
+  module St : Sort.SORT
+  type op
+
+  type valence = St.t list * St.t
+  type arity   = valence list * St.t
+
+  val ar : op -> arity
+  val equal : op * op -> bool
+  val to_string : op -> string
 end
